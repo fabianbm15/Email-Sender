b9644dc175562f22cfbd785642ba7bdc53d9c0b6
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2024-04-03 12:30:03 -0500

    Solved CORS

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var path = require("path");
 
 var indexRouter = require("./routes/index");
 var emailRouter = require("./routes/sendEmail");
@@ -16,6 +17,7 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
